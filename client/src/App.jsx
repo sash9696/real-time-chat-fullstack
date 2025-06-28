732453dcb21eb056b9d61a82fee77150493b9869
Sahil Chopra <sahilchopra838@gmail.com>
2025-06-28 14:55:34 +0530

    Update client components and add Start component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter , Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import AuthForm from "./pages/Register"
+import Start from "./components/Start"
 function App() {
   return (
     <div className="bg-[#F8F4EA]">
@@ -11,7 +12,7 @@ function App() {
         <Route path="/login" element={<AuthForm mode='login'/>} />
         <Route path="/register" element={<AuthForm mode='register' />} />
         <Route path="/chats" element={<Home />} />
-        {/* <Route path="/" element={<Start />} /> */}
+        <Route path="/" element={<Start />} />
 
 
 
